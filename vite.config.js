719511614651cd0eb113370f27495f1389e9565d
JRA-zachpennington <90287368+JRA-zachpennington@users.noreply.github.com>
2024-02-25 03:48:32 -0600

    vite manifest

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,22 @@ import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import Mkcert from 'vite-plugin-mkcert'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     Mkcert(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Hide-n-Seek',
+        short_name: 'Hide-n-Seek',
+        description: 'AR Game Template',
+        theme_color: '#ffffff',
+      }
+    })
   ],
 
   base: "/ar-game-template/",
